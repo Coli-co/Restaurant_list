5f5216d2c13d5065784527c2f7ff5460dc57828b
Coli-co <awestlifeblue@gmail.com>
2022-09-19 15:38:04 +0800

    Create search function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars')
 // import json data
 const restaurantList = require('./restaurantList.json')
 
+
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -13,8 +14,11 @@ app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('public'))
 
+
+// show all restaurant router
 app.get('/', (req, res) => {
   res.render('index', { restaurant: restaurantList.results})
+
 })
 
 
@@ -29,13 +33,20 @@ app.get('/restaurant/list',(req, res) => {
   res.send('This is good restaurant list.')
 })
 
-
+// show each restaurant router
 app.get('/restaurantlist/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', {each_restaurant: restaurant})
 })
 
-
+// search restaurant router
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const search_restaurant = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase())
+  })
+  res.render('index', {restaurant: search_restaurant, keyword: keyword})
+})
 
 
 
