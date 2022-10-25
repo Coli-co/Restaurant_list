798685e691d8b4c6c12b57544ecc90fe4e64cf12
Coli-co <awestlifeblue@gmail.com>
2022-10-25 20:06:51 +0800

    add message partials and message hint set

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 require('./config/mongoose')
 const usePassport = require('./config/passport')
-const { use } = require('./routes')
 
 const app = express()
 const port = 3000
@@ -27,9 +27,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
