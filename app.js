96fd7c9e24776e57b526e3712301fde7b0697a21
Coli-co <awestlifeblue@gmail.com>
2022-10-25 11:51:22 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
@@ -7,18 +8,22 @@ require('./config/mongoose')
 const app = express()
 const port = 3000
 
-// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-// setting static files
+
 app.use(express.static('public'))
-// every request through body-parser process
+app.use(
+  session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
 app.use(bodyParser.urlencoded({ extended: true }))
-// every request through methodOveride process
 app.use(methodOverride('_method'))
 app.use(routes)
 
-// Listening on server
 app.listen(port, () => {
   console.log(`Express is running on http://localhost/${port}`)
 })
