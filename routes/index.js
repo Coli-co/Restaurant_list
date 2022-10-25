eab091b308deeed80fbc3f6f297b4ecd9a81a1f3
Coli-co <awestlifeblue@gmail.com>
2022-10-25 15:09:27 +0800

    feat: add user authentication in restaurant and home routers

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/restaurants', restaurant)
+router.use('/restaurants', authenticator, restaurant)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
