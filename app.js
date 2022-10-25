af344902d8095fe7045e745447a1ca14bb1ff3c3
Coli-co <awestlifeblue@gmail.com>
2022-10-25 14:34:57 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
+const usePassport = require('./config/passport')
+const { use } = require('./routes')
+
 const app = express()
 const port = 3000
 
@@ -22,6 +25,9 @@ app.use(
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)
 
 app.listen(port, () => {
